9e5c15198608a772eef92ce9c90e4a165c5da7dc
FaroukAjimi <benajimi@gmail.com>
2019-09-24 11:06:05 +0000

    dfdfd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * a - check the code for Holberton School students.
+ *@a: fdfdfd
+ *@b: fdfdfd
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * times_table - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+int i;
+
