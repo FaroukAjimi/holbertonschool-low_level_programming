315cc39f4052785ef0e7bb6f974fc637d8b61fdb
FaroukAjimi <benajimi@gmail.com>
2019-09-24 02:47:42 +0000

    dfdfd

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_last_digit - check the code for Holberton School students.
+ *@x: dffdf
+ * Return: Always 0.
+ */
+int print_last_digit(int x)
+{
+int n;
+
+n = x % 10;
+if (x >= 48)
+{
+_putchar(n + '0');
+return (n);
+}
+else
+{
+_putchar(n * -1 + '0');
+return (n * -1);
+}
+}
