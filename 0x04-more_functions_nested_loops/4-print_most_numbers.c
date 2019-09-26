b46223b77851a9896a6c3f82b4a6384be9212331
FaroukAjimi <benajimi@gmail.com>
2019-09-26 13:18:56 +0000

    dfdfd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_most_numbers - check the code for Holberton School students.
  *
  * Return: adsds
  */
 void print_most_numbers(void)
 {
 	char a = '0';
+
 	while (a <= '9')
 	{
-		if (a != '0' && a!= '9')
+		if (a != '0' && a != '9')
 		{
 		_putchar(a);
 		}
