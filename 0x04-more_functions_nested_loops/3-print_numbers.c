ddaed563e6040f86cb3208d992f631c78a206b9f
FaroukAjimi <benajimi@gmail.com>
2019-09-26 13:06:41 +0000

    fdfdfd

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+char a = '0';
+while (a <= '9')
+{
+	_putchar(a);
+	a++;
+}
+_putchar ('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: adsds
+ */
+void print_most_numbers(void)
+{
+	char a = '0';
+	while (a <= '9')
+	{
+		if (a != '0' && a!= '9')
+		{
+		_putchar(a);
+		}
+		a++;
+	}
+	_putchar ('\n');
+}
