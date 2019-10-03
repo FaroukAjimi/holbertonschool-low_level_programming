496c3f79d0ebfeb548f5f19e4deb3535063f863b
FaroukAjimi <benajimi@gmail.com>
2019-10-03 19:39:15 +0000

    dfdfe

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,26 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-
-	while (i <= n && src[i] != '\0')
+	int y = 0;
+	int z = 0;
+	while (dest[y] != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		y++;
+	}
+	while (src[z] != '\0')
+	{
+		z++;
 	}
 	while (i < n)
 	{
-		dest[i] = '\0';
+		if (src[n - 1] != '\0')
+			dest[i] = src[i];
+		if (i > sl)
+			dest[i] = '\0';
+		else
+		{
+			dest[i] = src[i];
+		}
 		i++;
 	}
 	return (dest);
