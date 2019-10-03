13b7f70c821a7ba4d516b6b9d5d058646d1598aa
FaroukAjimi <benajimi@gmail.com>
2019-10-03 19:13:54 +0000

    fdfdf

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *string_toupper(char *s)
+{
+	int i=0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <=122)
+	{
+		s[i]-=32;
+	}
+		i++;
+	}
+
+	return (s);
+}
