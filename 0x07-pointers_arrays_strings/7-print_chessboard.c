fedaf9c49a264468c4790f825ace4ee88e248862
FaroukAjimi <benajimi@gmail.com>
2019-10-07 22:27:13 +0000

    dfdfd

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * print_chessboard - check the code for Holberton School students.
+ *@a: fidjfkldjfd
  * Return: Always 0.
  */
 void print_chessboard(char (*a)[8])
 {
-	int i=0;
-	int y=0;
+	int i = 0;
+	int y = 0;
+
 	while (i < 8)
 	{
-		y=0;
+		y = 0;
 		while (y < 8)
 		{
 			_putchar(a[i][y]);
