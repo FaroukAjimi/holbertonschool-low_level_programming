2992e5b6d1e1f3bff30cb1f1a6f58098d162783f
FaroukAjimi <benajimi@gmail.com>
2019-10-07 22:19:09 +0000

    zdfdfd

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,29 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _strspn - check the code for Holberton School students.
+ *@s: rfkjdgfhjdk
+ *@accept: fjkdfhjdkf
  * Return: Always 0.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i=0;
-	int y=0;
-	int n=0;
-	int z=0;
+	int i = 0;
+	int y = 0;
+	int n = 0;
+	int z = 0;
+
 	while (s[i] != ' ' && s[i] != '\0')
 	{
 		i++;
 	}
 	while (y <= i)
 	{
-		z=0;
+		z = 0;
 		while (accept[z] != '\0')
 		{
 			if (s[y] == accept[z])
-				n=n+1;
+				n = n + 1;
 			z++;
 		}
 		y++;
