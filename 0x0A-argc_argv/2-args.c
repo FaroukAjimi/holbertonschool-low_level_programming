8d2a92467587dc5e2c60a09d2275e74476528c64
FaroukAjimi <benajimi@gmail.com>
2019-10-12 13:24:08 +0000

    dfdf

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+/**
+ *main - hello
+ *@argc: arg1
+ *@argv: fdfdfdf
+ *Return: dsdsds
+ */
+int main(int argc, char *argv[])
 {
-	int count;
+        int i;
 
-	printf ("This program was called with \"%s\".\n",argv[0]);
-
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-		{
-			printf("%s",  argv[count]);
-		}
-	}
-	else
-	{
-		printf("The command had no other arguments.\n");
-	}
-
-	return 0;
+        for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+        return (0);
 }
