835f76a09c76875ec26c91cd81d2f201a45dc65f
FaroukAjimi <benajimi@gmail.com>
2019-10-16 17:43:08 +0000

    come on dude

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,3 +12,28 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int **s;
+	int i;
+	int f;
+	int y;
+
+	s = malloc(sizeof(int *) * height);
+	if (s == '\0')
+		return (NULL);
+	for (i=0; i< height;i++)
+	{
+			s[i] = malloc(sizeof(int) * width);
+			if (s[i] == 0)
+			{
+				for (f = 0 ; f < i ; f++)
+					free(s[i]);
+				free(s);
+				return (NULL);
+				for (y = 0; y < width; y++)
+					s[i][y] = 0;
+			}
+	}
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	return (s);
+}
