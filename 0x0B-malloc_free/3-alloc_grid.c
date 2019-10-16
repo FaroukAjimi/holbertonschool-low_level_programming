6ff8e533146f701bb685e31b437cd3bd7fffa2c4
FaroukAjimi <benajimi@gmail.com>
2019-10-16 17:57:04 +0000

    Farah go

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,23 +15,23 @@ int **alloc_grid(int width, int height)
 	int f;
 	int y;
 
-	if (width <= 0 || height <= 0)
-                return (NULL);
-	s = malloc(sizeof(int *) * height);
-	if (s == '\0')
-		return (NULL);
-	for (i = 0 ; i < height ; i++)
+if (width <= 0 || height <= 0)
+return (NULL);
+s = malloc(sizeof(int *) * height);
+if (s == '\0')
+	return (NULL);
+for (i = 0 ; i < height ; i++)
+{
+	s[i] = malloc(sizeof(int) * width);
+	if (s[i] == 0)
 	{
-		s[i] = malloc(sizeof(int) * width);
-		if (s[i] == 0)
-		{
-			for (f = 0 ; f < i ; f++)
-				free(s[i]);
-			free(s);
-			return (NULL);
-			for (y = 0; y < width; y++)
-				s[i][y] = 0;
-		}
+		for (f = 0 ; f < i ; f++)
+			free(s[i]);
+		free(s);
+		return (NULL);
+		for (y = 0; y < width; y++)
+			s[i][y] = 0;
 	}
-	return (s);
+}
+return (s);
 }
