e484026c81c2f4e4a2ac5e11905b532ff026dd98
FaroukAjimi <benajimi@gmail.com>
2019-10-21 14:01:40 +0000

    HAH

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Short descriptio
+ * @name: First member
+ * @age: Second member
+ * @owner: Third memb
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
