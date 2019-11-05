5b76221f6f84f194a60d8ef33f01b3a985a33357
FaroukAjimi <benajimi@gmail.com>
2019-11-05 18:31:15 +0000

    ooii

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,14 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-
-
+	unsigned int i = 0;
+	listint_t *h = head;
+	if (head == NULL)
+		return (NULL);
+	while (i < index && h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (h);
 }
