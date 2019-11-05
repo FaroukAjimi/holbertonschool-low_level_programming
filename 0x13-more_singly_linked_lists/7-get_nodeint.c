d46c79c1a6fa100d26bd0b5a2885c34f7447cdf5
FaroukAjimi <benajimi@gmail.com>
2019-11-05 18:43:48 +0000

    Fix BEtty

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,14 +4,16 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * get_nodeint_at_index - check the code for Holberton School students.
+ *@head: popl
+ *@index: lomp
  * Return: Always 0.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *h = head;
+
 	if (head == NULL)
 		return (NULL);
 	while (i < index && h != NULL)
