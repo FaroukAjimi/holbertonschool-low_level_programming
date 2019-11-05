5fea35e87bb3d5f3d8caf75330e81d01a7eaef46
FaroukAjimi <benajimi@gmail.com>
2019-11-05 17:51:13 +0000

    ooods

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,6 +14,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t **tracer = head, *new;
 
 	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
 	while (*tracer)
 	{
 		tracer = &(*tracer)->next;
