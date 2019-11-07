23ef5e7f9a12a84a1fbe0acb8b12675d59e25e83
FaroukAjimi <benajimi@gmail.com>
2019-11-07 20:33:17 +0000

    kkk

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+	if (index > (sizeof(n) * 8))
+		return (-1);
+	i = 1 << index;
+	if (n & i)
+		return (1);
+	else
+		return (0);
+}
