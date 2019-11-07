50dacaf49cf9486d31326d8c72c196a0b810dd48
FaroukAjimi <benajimi@gmail.com>
2019-11-07 18:18:50 +0000

    op

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,20 +8,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int mask = sizeof(unsigned long int);
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	mask = n;
-	while (mask > 0)
-	{
-		if ((n & mask) == 0)
-			_putchar('0');
-		else
-			_putchar('1');
-		mask = mask >> 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + ('0'));
 }
