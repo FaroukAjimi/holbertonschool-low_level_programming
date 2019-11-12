9f29a36e4e8ce35af93826438e66127986e58dc2
FaroukAjimi <benajimi@gmail.com>
2019-11-12 19:31:57 +0000

    kkk

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * read_textfile - check the code for Holberton School students.
+ *@filename: ooof
+ *@letters: ppppf
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *s;
+	ssize_t p, w;
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	s = malloc(letters * sizeof(char));
+	if (s == NULL)
+		return (0);
+	p = read(fd, s, letters);
+	if (p == -1)
+		return (0);
+	w = write(STDOUT_FILENO, s, p);
+	if (w == -1 || p != w)
+		return (0);
+	return (w);
+}
