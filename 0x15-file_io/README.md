39189c9360b4336db5ab5cf03a19a7d07f3d3261
FaroukAjimi <benajimi@gmail.com>
2019-11-12 18:49:02 +0000

    lll

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This project is about the file inputs and outputs
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
