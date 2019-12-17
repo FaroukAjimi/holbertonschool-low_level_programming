68a66527faf702fa2bc5f84c128d881697747fd4
FaroukAjimi <benajimi@gmail.com>
2019-12-17 20:20:08 +0000

    llll

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,11 +12,11 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	dlistint_t k = head;
+	dlistint_t *k = head;
 
 	while (k != NULL)
 	{
-		if (index == k)
+		if (index == i)
 			return (k);
 		k = k->next;
 		i++;
