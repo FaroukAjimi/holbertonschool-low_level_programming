f085fcd3de698e183a5dc891e25cf8aa3d52a8fb
FaroukAjimi <benajimi@gmail.com>
2019-12-17 20:11:05 +0000

    mlk

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - check the code for Holberton School students.
+ *@head: df
+ *@index: fd
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t k = head;
+
+	while (k != NULL)
+	{
+		if (index == k)
+			return (k);
+		k = k->next;
+		i++;
+	}
+	return (NULL);
+}
