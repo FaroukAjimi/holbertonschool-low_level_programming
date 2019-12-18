b8ce355332f15e6a9ece37e773acf95f42f23eb3
FaroukAjimi <benajimi@gmail.com>
2019-12-18 16:29:42 +0000

    mml

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *mul - multiply
+ *@a: fd
+ *@b: ds
+ *Return: VALUE
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * sub - check the code for Holberton School stude
+ *@a: fdfd
+ *@b: fdfdfd
+ * Return: Always 0.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
