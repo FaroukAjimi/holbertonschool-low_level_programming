2170d85fe577879ef73eb11a3ccb5f8ebfe09dd1
FaroukAjimi <benajimi@gmail.com>
2020-04-16 18:31:52 +0100

    back to C'

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - Entry point
+ * @array: array of integer that would be searched
+ * @size: size of array
+ * @value: the value to search for
+ * Return: Always EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value)
+{
+size_t i;
+for (i = 0; i <= size; i++)
+{
+if (array[i] == value)
+{
+return (i);
+}
+}
+return (-1);
+}
